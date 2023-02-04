1c8b763a94f91d16151a6a9be75f0597ae052dc5
Sunnythesun <sunnyliang@live.com.au>
2023-02-05 00:50:15 +1100

    library array loop

--- javascript.js
+++ javascript.js
@@ -7,30 +7,33 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 
     this.info = function () {
-        console.log(title + " by " + author + ", " + pages + ", " + readStatus)
+        // console.log(title + " by " + author + ", " + pages + ", " + readStatus);
+        document.getElementById("library").innerHTML += (title + " by " + author + ", " + "Pages: " + pages + ", " + "Status: " + readStatus + "<br>");
     }
 
+}
+
+function inputData() {
+    var inputTitle = document.getElementById('inputTitle').value;
+    var inputAuthor = document.getElementById('inputAuthor').value;
+    var inputPages = document.getElementById('inputPages').value;
+    var inputStatus = document.getElementById('inputStatus').value;
 }
 
 const shogun = new Book("'Shogun'", "James Clavell", "1152", "Read");
 const taipan = new Book("'Tai-Pan'", "James Clavell", "727", "Read");
 const gaijin = new Book("'Gai-Jin'", "James Clavell", "1126", "Not Read");
+// const input = new Book(inputTitle, inputAuthor, inputPages, inputStatus);
 
 myLibrary.push(shogun);
 myLibrary.push(taipan);
 myLibrary.push(gaijin);
+// myLibrary.push(input);
 
-
-shogun.info();
-taipan.info();
-gaijin.info();
 
 for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+    myLibrary[i].info();
 }
 
-// function addBookToLibrary() {
-//     console.log(Shogun.title);
-// }
 
 
