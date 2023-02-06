5afa6ef09674e366abba48891d298ec229004b46
Sunnythesun <sunnyliang@live.com.au>
2023-02-07 00:05:35 +1100

    add book using form

--- javascript.js
+++ javascript.js
@@ -1,39 +1,45 @@
 let myLibrary = [];
 
+const form = document.querySelector('#form');
+const inputTitle = document.querySelector('#inputTitle');
+const inputAuthor = document.querySelector('#inputAuthor');
+const inputPages = document.querySelector('#inputPages');
+
 function Book(title, author, pages, readStatus) {
-    this.title - title;
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
 
     this.info = function () {
-        // console.log(title + " by " + author + ", " + pages + ", " + readStatus);
         document.getElementById("library").innerHTML += (title + " by " + author + ", " + "Pages: " + pages + ", " + "Status: " + readStatus + "<br>");
     }
 
 }
 
-function inputData() {
-    var inputTitle = document.getElementById('inputTitle').value;
-    var inputAuthor = document.getElementById('inputAuthor').value;
-    var inputPages = document.getElementById('inputPages').value;
-    var inputStatus = document.getElementById('inputStatus').value;
-}
-
 const shogun = new Book("'Shogun'", "James Clavell", "1152", "Read");
 const taipan = new Book("'Tai-Pan'", "James Clavell", "727", "Read");
 const gaijin = new Book("'Gai-Jin'", "James Clavell", "1126", "Not Read");
-// const input = new Book(inputTitle, inputAuthor, inputPages, inputStatus);
 
 myLibrary.push(shogun);
 myLibrary.push(taipan);
 myLibrary.push(gaijin);
-// myLibrary.push(input);
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const inputBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, "Not Read");
+    myLibrary.push(inputBook);
+
+    document.getElementById("library").innerHTML = "";
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        myLibrary[i].info();
+    }
+
+});
 
 for (let i = 0; i < myLibrary.length; i++) {
     myLibrary[i].info();
 }
-
-
 
